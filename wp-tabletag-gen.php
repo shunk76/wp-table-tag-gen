802561ca6d05a1d9c3b5fc00d4b0ff15e04dfe35
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2014-07-09 00:16:49 +0900

    commit

--- wp-tabletag-gen.php
+++ wp-tabletag-gen.php
@@ -8,73 +8,84 @@ Author: Shunsuke Kusakabe
 
 /*  Copyright 2014 Shunsuke Kusakabe (email : shunsuke.kusakabe@gmail.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-		published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-add_action( 'admin_footer', 'addTTGFiles' );
-add_action( 'admin_footer', 'insertTTG');
+$file = basename($_SERVER['SCRIPT_NAME']);
+
+if( $file == 'post-new.php' ||  $file == 'post.php' ){
+    add_action( 'admin_footer', 'addTTGFiles' );
+    add_action( 'admin_footer', 'insertTTG');
+    load_plugin_textdomain( 'wp-tabletag-gen', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
 
 function addTTGFiles(){
-	$plugin = 'wp-tabletag-gen';
-    wp_enqueue_style( $plugin, WP_PLUGIN_URL.'/'.$plugin.'/style.css', false, '1.0', 'all' );
-    wp_enqueue_script( 'addButton', WP_PLUGIN_URL.'/'.$plugin.'/js/addButton.js', array('jquery'), '1.0', true );
-    wp_enqueue_script( 'mousewheel', WP_PLUGIN_URL.'/'.$plugin.'/js/jquery.mousewheel.min.js', array('jquery'), '3.1.11', true );
-    wp_enqueue_script( 'cookie', WP_PLUGIN_URL.'/'.$plugin.'/js/jquery.cookie.js', array('jquery'), '1.0', true );
-    wp_enqueue_script( $plugin, WP_PLUGIN_URL.'/'.$plugin.'/js/script.js', array('jquery-ui-selectable'), '1.0', true );
+    $plugin = 'wp-tabletag-gen';
+    wp_enqueue_style( $plugin, WP_PLUGIN_URL.'/'.$plugin.'/css/style.min.css', false, '1.0', 'all' );
+    wp_enqueue_script( $plugin, WP_PLUGIN_URL.'/'.$plugin.'/js/'.$plugin.'.js', array('jquery-ui-selectable'), '1.0', true );
     add_thickbox();
 }
 
 function insertTTG(){
 ?>
-<div style="display: none;">
-  <div id="tableTagGen">
+<div id="tableTagGen" style="display: none;">
     <div id="generator">
-      <header>
-        行<input class="value" type="text" name="row" value="3" size="1">
-        <button id="row_up" class="button">＋</button>
-        <button id="row_down" class="button">－</button> &times;
-        列<input class="value" type="text" name="col" value="3" size="1">
-        <button id="col_up" class="button">＋</button>
-        <button id="col_down" class="button">－</button>
+        <header>
+            <?php _e( 'row', 'wp-tabletag-gen' ); ?><input type="number" name="row" value="3" min="1"> &times;
+            <?php _e( 'col', 'wp-tabletag-gen' ); ?><input type="number" name="col" value="3" min="1">
 
-        行<input type="number" name="" style="width: 3em;" id="gyo" value="3" min="1"> &times;
-        列<input type="number" name="" style="width: 3em;" id="retu" value="3" min="1">
+            <button id="merge" class="button" title="<?php _e( 'Merge cells', 'wp-tabletag-gen' ); ?>"><?php _e( 'Merge', 'wp-tabletag-gen' ); ?></button>
+            <button id="split" class="button" title="<?php _e( 'Split the merged cell', 'wp-tabletag-gen' ); ?>"><?php _e( 'Split', 'wp-tabletag-gen' ); ?></button>
+            <button id="replace" class="button" title="<?php _e( 'Replace &lt;td&gt; and &lt;th&gt; each other', 'wp-tabletag-gen' ); ?>">td &hArr; th</button>
+            <button id="chars" class="button" title="<?php _e( 'Enter the characters', 'wp-tabletag-gen' ); ?>"><?php _e( 'Chars', 'wp-tabletag-gen' ); ?></button>
+            <button id="outputChars" class="button" title="<?php _e( 'Output the characters', 'wp-tabletag-gen' ); ?>"><?php _e( 'Output', 'wp-tabletag-gen' ); ?></button>
+            <button id="class" class="button" title="<?php _e( 'Enter the name of the class', 'wp-tabletag-gen' ); ?>">class</button>
+            <button id="outputClass" class="button" title="<?php _e( 'Output the name of the class', 'wp-tabletag-gen' ); ?>"><?php _e( 'Output', 'wp-tabletag-gen' ); ?></button>
+            <button id="undo" class="button" title="<?php _e( 'Undo', 'wp-tabletag-gen' ); ?>" disabled="disabled"><i class="dashicons dashicons-undo"></i></button>
+            <button id="redo" class="button" title="<?php _e( 'Redo', 'wp-tabletag-gen' ); ?>" disabled="disabled"><i class="dashicons dashicons-redo"></i></button>
+            <button id="initialize" class="button" title="<?php _e( 'initialize', 'wp-tabletag-gen' ); ?>"><?php _e( 'Initialize', 'wp-tabletag-gen' ); ?></button>
+            <button id="insert" class="button button-primary"><?php _e( 'insert into post', 'wp-tabletag-gen' ); ?></button>
+        </header>
 
-        <button id="combine" class="button" title="セルを結合する">結合</button>
-        <button id="split" class="button" title="結合されたセルを分割する">分割</button>
-        <button id="convert_th" class="button" title="td を th に変換する">td→th</button>
-        <button id="convert_td" class="button" title="th を td に変換する">th→td</button>
-        <button id="text" class="button" title="文字を入力する">文字</button>
-        <button id="output_text" class="button" title="文字を出力する">出力</button>
-        <button id="class" class="button" title="クラス名を入力する">class</button>
-        <button id="output_class" class="button" title="クラス名を出力する">出力</button>
-        <button id="undo" class="button" title="1つ戻る" disabled="disabled">←</button>
-        <button id="redo" class="button" title="1つ進む" disabled="disabled">→</button>
-        <button id="initialize" class="button" title="初期化する">初期化</button>
-        <button id="insert" class="button button-primary">投稿に挿入</button>
-      </header>
+        <div class="message">
+            <p id="selectByDragging" class="default"><?php _e( 'You can select the cells by dragging.', 'wp-tabletag-gen' ); ?></p>
+            <p id="entChars"><?php _e( 'Please enter the characters.', 'wp-tabletag-gen'); ?></p>
+            <p id="entClassNames"><?php _e( 'Please enter the name of the class.', 'wp-tabletag-gen'); ?></p>
+            <p id="cantMerge" class="alert">
+                <i class="dashicons dashicons-no"></i><?php _e( 'It is not possible to marge &lt;td&gt; and &lt;th&gt;.', 'wp-tabletag-gen' ); ?>
+            </p>
+            <p id="cantRemerge" class="alert">
+                <i class="dashicons dashicons-no"></i><?php _e( 'It is not possible to re-merge the merged cells.', 'wp-tabletag-gen' ); ?>
+            </p>
+            <p id="entNum" class="alert">
+                <i class="dashicons dashicons-no"></i><?php _e( 'Please enter a number.', 'wp-tabletag-gen' ); ?>
+            </p>
+            <p id="entNaturalNum" class="alert">
+                <i class="dashicons dashicons-no"></i><?php _e( 'Please enter a natural number(1,2,3...).', 'wp-tabletag-gen' ); ?>
+            </p>
+            <p id="selectCells" class="alert">
+                <i class="dashicons dashicons-no"></i><?php _e( 'To merge the cells, please select the cells by dragging.', 'wp-tabletag-gen' ); ?>
+            </p>
 
-      <div id="main">
-        <p class="ja">ドラッグしてセルを選択することができます。</p>
+        </div>
+
         <div id="tagWrapper"><table><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></table></div>
-      </div>
 
-      <div id="source" style="display: none;">
-        <textarea name="source" rows="40" cols="40"></textarea>
-      </div>
+        <div style="display: none;">
+            <textarea name="source" rows="40" cols="40"></textarea>
+        </div>
     </div><!-- /generator -->
-  </div>
 </div>
 <?php
 }
