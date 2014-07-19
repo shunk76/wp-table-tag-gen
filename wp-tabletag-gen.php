5b1589f9a3cfc37157fd3e3c921db9c74d36a8f4
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2014-07-19 22:59:55 +0900

    kaizen

--- wp-tabletag-gen.php
+++ wp-tabletag-gen.php
@@ -22,18 +22,27 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+define( 'TTG_DEBUG', false );
+define( 'WP_TTG', 'wp-tabletag-gen' );
+
 $file = basename($_SERVER['SCRIPT_NAME']);
 
 if( $file == 'post-new.php' ||  $file == 'post.php' ){
     add_action( 'admin_footer', 'addTTGFiles' );
     add_action( 'admin_footer', 'insertTTG');
-    load_plugin_textdomain( 'wp-tabletag-gen', false, basename( dirname( __FILE__ ) ) . '/languages' );
+    load_plugin_textdomain( WP_TTG, false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 function addTTGFiles(){
-    $plugin = 'wp-tabletag-gen';
-    wp_enqueue_style( $plugin, WP_PLUGIN_URL.'/'.$plugin.'/css/style.min.css', false, '1.0', 'all' );
-    wp_enqueue_script( $plugin, WP_PLUGIN_URL.'/'.$plugin.'/js/'.$plugin.'.js', array('jquery-ui-selectable'), '1.0', true );
+    wp_enqueue_style( WP_TTG, WP_PLUGIN_URL.'/'.WP_TTG.'/css/style.min.css', false, '1.0', 'all' );
+
+    $js_file = WP_PLUGIN_URL.'/'.WP_TTG.'/js/'.WP_TTG;
+    if( TTG_DEBUG == false ){
+        $js_file .= '.min.js';
+    } else {
+        $js_file .= '.js';
+    }
+    wp_enqueue_script( WP_TTG, $js_file, array('jquery-ui-selectable'), '1.0', true );
     add_thickbox();
 }
 
@@ -42,46 +51,46 @@ function insertTTG(){
 <div id="tableTagGen" style="display: none;">
     <div id="generator">
         <div class="ttg-header">
-            <?php _e( 'row', 'wp-tabletag-gen' ); ?><input type="number" name="row" value="3" min="1"> &times;
-            <?php _e( 'col', 'wp-tabletag-gen' ); ?><input type="number" name="col" value="3" min="1">
+            <?php _e( 'row', WP_TTG ); ?><input type="number" name="row" value="3" min="1"> &times;
+            <?php _e( 'col', WP_TTG ); ?><input type="number" name="col" value="3" min="1">
             <div class="help">
                 <div class="dashicons dashicons-editor-help"></div>
                 <div class="content">
-                    <?php _e( 'To increase or decrease the value, you can either enter on the keyboard, click on the spinner, or turn the mouse wheel.', 'wp-tabletag-gen' ); ?>
+                    <?php _e( 'To increase or decrease the value, you can either enter on the keyboard, click on the spinner, or turn the mouse wheel.', WP_TTG ); ?>
                 </div>
             </div>
 
-            <button id="merge" class="button" title="<?php _e( 'Merge cells', 'wp-tabletag-gen' ); ?>"><?php _e( 'Merge', 'wp-tabletag-gen' ); ?></button>
-            <button id="split" class="button" title="<?php _e( 'Split the merged cell', 'wp-tabletag-gen' ); ?>"><?php _e( 'Split', 'wp-tabletag-gen' ); ?></button>
-            <button id="replace" class="button" title="<?php _e( 'Replace &lt;td&gt; and &lt;th&gt; each other', 'wp-tabletag-gen' ); ?>">td &hArr; th</button>
-            <button id="chars" class="button" title="<?php _e( 'Enter the characters', 'wp-tabletag-gen' ); ?>"><?php _e( 'Chars', 'wp-tabletag-gen' ); ?></button>
-            <button id="outputChars" class="button" title="<?php _e( 'Output the characters', 'wp-tabletag-gen' ); ?>"><?php _e( 'Output', 'wp-tabletag-gen' ); ?></button>
-            <button id="class" class="button" title="<?php _e( 'Enter the name of the class', 'wp-tabletag-gen' ); ?>">class</button>
-            <button id="outputClass" class="button" title="<?php _e( 'Output the name of the class', 'wp-tabletag-gen' ); ?>"><?php _e( 'Output', 'wp-tabletag-gen' ); ?></button>
-            <button id="undo" class="button" title="<?php _e( 'Undo', 'wp-tabletag-gen' ); ?>" disabled="disabled"><i class="dashicons dashicons-undo"></i></button>
-            <button id="redo" class="button" title="<?php _e( 'Redo', 'wp-tabletag-gen' ); ?>" disabled="disabled"><i class="dashicons dashicons-redo"></i></button>
-            <button id="initialize" class="button" title="<?php _e( 'initialize', 'wp-tabletag-gen' ); ?>"><?php _e( 'Initialize', 'wp-tabletag-gen' ); ?></button>
-            <button id="insert" class="button button-primary"><?php _e( 'insert into post', 'wp-tabletag-gen' ); ?></button>
+            <button id="merge" class="button" title="<?php _e( 'Merge cells', WP_TTG ); ?>"><?php _e( 'Merge', WP_TTG ); ?></button>
+            <button id="split" class="button" title="<?php _e( 'Split the merged cell', WP_TTG ); ?>"><?php _e( 'Split', WP_TTG ); ?></button>
+            <button id="replace" class="button" title="<?php _e( 'Replace &lt;td&gt; and &lt;th&gt; each other', WP_TTG ); ?>">td &hArr; th</button>
+            <button id="chars" class="button" title="<?php _e( 'Enter the characters', WP_TTG ); ?>"><?php _e( 'Chars', WP_TTG ); ?></button>
+            <button id="outputChars" class="button" title="<?php _e( 'Output the characters', WP_TTG ); ?>"><?php _e( 'Output', WP_TTG ); ?></button>
+            <button id="class" class="button" title="<?php _e( 'Enter the name of the class', WP_TTG ); ?>">class</button>
+            <button id="outputClass" class="button" title="<?php _e( 'Output the name of the class', WP_TTG ); ?>"><?php _e( 'Output', WP_TTG ); ?></button>
+            <button id="undo" class="button" title="<?php _e( 'Undo', WP_TTG ); ?>" disabled="disabled"><i class="dashicons dashicons-undo"></i></button>
+            <button id="redo" class="button" title="<?php _e( 'Redo', WP_TTG ); ?>" disabled="disabled"><i class="dashicons dashicons-redo"></i></button>
+            <button id="initialize" class="button" title="<?php _e( 'initialize', WP_TTG ); ?>"><?php _e( 'Initialize', WP_TTG ); ?></button>
+            <button id="insert" class="button button-primary"><?php _e( 'insert into post', WP_TTG ); ?></button>
         </div>
 
         <div class="message">
-            <p id="selectByDragging" class="default"><?php _e( 'You can select the cells by dragging.', 'wp-tabletag-gen' ); ?></p>
-            <p id="entChars"><?php _e( 'Please enter the characters.', 'wp-tabletag-gen'); ?></p>
-            <p id="entClassNames"><?php _e( 'Please enter the name of the class.', 'wp-tabletag-gen'); ?></p>
+            <p id="selectByDragging" class="default"><?php _e( 'You can select the cells by dragging.', WP_TTG ); ?></p>
+            <p id="entChars"><?php _e( 'Please enter the characters.', WP_TTG); ?></p>
+            <p id="entClassNames"><?php _e( 'Please enter the name of the class.', WP_TTG); ?></p>
             <p id="cantMerge" class="alert">
-                <i class="dashicons dashicons-no"></i><?php _e( 'It is not possible to marge &lt;td&gt; and &lt;th&gt;.', 'wp-tabletag-gen' ); ?>
+                <i class="dashicons dashicons-no"></i><?php _e( 'It is not possible to marge &lt;td&gt; and &lt;th&gt;.', WP_TTG ); ?>
             </p>
             <p id="cantRemerge" class="alert">
-                <i class="dashicons dashicons-no"></i><?php _e( 'It is not possible to re-merge the merged cells.', 'wp-tabletag-gen' ); ?>
+                <i class="dashicons dashicons-no"></i><?php _e( 'It is not possible to re-merge the merged cells.', WP_TTG ); ?>
             </p>
             <p id="entNum" class="alert">
-                <i class="dashicons dashicons-no"></i><?php _e( 'Please enter a number.', 'wp-tabletag-gen' ); ?>
+                <i class="dashicons dashicons-no"></i><?php _e( 'Please enter a number.', WP_TTG ); ?>
             </p>
             <p id="entNaturalNum" class="alert">
-                <i class="dashicons dashicons-no"></i><?php _e( 'Please enter a natural number (1,2,3...).', 'wp-tabletag-gen' ); ?>
+                <i class="dashicons dashicons-no"></i><?php _e( 'Please enter a natural number (1,2,3...).', WP_TTG ); ?>
             </p>
             <p id="selectCells" class="alert">
-                <i class="dashicons dashicons-no"></i><?php _e( 'Please select the cells to be operated by dragging.', 'wp-tabletag-gen' ); ?>
+                <i class="dashicons dashicons-no"></i><?php _e( 'Please select the cells to be operated by dragging.', WP_TTG ); ?>
             </p>
 
         </div>
