4b3851fa3230d4457c9128dc8c35066895751c91
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2014-08-05 00:43:56 +0900

    bug fix

--- wp-table-tag-gen.php
+++ wp-table-tag-gen.php
@@ -8,7 +8,7 @@
  * Text Domain: wp-table-tag-gen
  * Domain Path: /languages
  *
- * Copyright 2014 Shunsuke Kusakabe (email : shunsuke.kusakabe@gmail.com)
+ * Copyright 2014 Shunsuke Kusakabe
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
